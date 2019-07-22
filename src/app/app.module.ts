3a538a7789b267f2cb657e66958f9520b7fb7a3d
NaveenKumar <naveenkumar.andi@gmail.com>
2019-07-22 18:15:38 +0530

    Student Registration Form Forst Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StudentRegistrationFormComponent } from './student-registration-form/student-registration-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StudentRegistrationFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
