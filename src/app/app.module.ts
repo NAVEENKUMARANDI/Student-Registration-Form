38b5b59b667e8cf30f2bd86575d9ea8e015c9be8
NaveenKumar <naveenkumar.andi@gmail.com>
2019-07-23 18:26:31 +0530

    Student Registration Form Second Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentRegistrationFormComponent } from './student-registration-form/student-registration-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { StudentRegistrationFormComponent } from './student-registration-form/st
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/student-registration-form/student-registration-form.component.ts
+++ src/app/student-registration-form/student-registration-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormControl, FormGroup, FormArray, FormControlDirective } from '@angular/forms';
 @Component({
   selector: 'app-student-registration-form',
   templateUrl: './student-registration-form.component.html',
@@ -7,9 +8,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentRegistrationFormComponent implements OnInit {
 
+  studentProfileForm = new FormGroup({
+    
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    
+    dateOfBirth: new FormGroup({
+      day: new FormControl(''),
+      month: new FormControl(''),
+      year: new FormControl('')
+    }),
+    
+    emailId: new FormControl(''),
+    mobileNumber: new FormControl(''),
+    gender: new FormControl(''),
+    
+    address: new FormGroup({
+      street: new FormControl(''),
+      city:new FormControl(''),
+      pinCode: new FormControl(''),
+      state: new FormControl(''),
+      country: new FormControl('')
+    }),
+
+    hobbies: new FormArray([
+      Drawing: new FormArray[''],
+      singing: new FormArray[''],
+      dancing: new FormArray[''],
+      others: new FormArray['']
+    ]),
+
+    qualification: new FormGroup({
+      qualificationNumber: new FormControl(''),
+      exam: new FormControl(''),
+      board: new FormControl(''),
+      percentage: new FormControl(''),
+      yearOfPassing: new FormControl('')
+    }),
+
+    coursesApplied:new FormControl('')
+})
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    
+    console.warn(this.studentProfileForm.value);
   }
 
 }
